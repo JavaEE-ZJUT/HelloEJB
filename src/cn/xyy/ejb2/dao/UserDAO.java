df5b3484d2019e751152cb45cd0935f03f353aa5
xyy <xuyuyan999@126.com>
2019-12-15 23:35:31 +0800

    finish Wildfly Persistence settings & finish hello_word

---
+++ src/cn/xyy/ejb2/dao/UserDAO.java
@@ -0,0 +1,42 @@
+package cn.xyy.ejb2.dao;
+
+import java.util.List;
+import javax.ejb.Remote;
+import javax.ejb.Stateful;
+import javax.persistence.*;
+
+import cn.xyy.ejb.ProductCartRemote;
+import cn.xyy.ejb2.User;
+
+
+@Stateful(name = "UserDAOEJB")
+@Remote(UserDAORemote.class)
+public class UserDAO implements UserDAORemote {
+    @PersistenceContext(type=PersistenceContextType.EXTENDED, unitName="userpu")
+    EntityManager em;
+    public UserDAO() { }
+
+    @Override
+    public boolean insert(User user){
+        try{
+            em.persist(user);
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public List<User> select(String sql){
+        List< User> userlist=null;
+        try{
+            Query q=em.createQuery(sql);
+            userlist=(List<User>)q.getResultList();
+        }catch(Exception e){
+            e.printStackTrace();
+            return userlist;
+        }
+        return userlist;
+    }
+
+}
